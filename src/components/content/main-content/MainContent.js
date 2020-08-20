0faef7c459266361649d05f450c63c6ae3eaefdc
drdimaaws <drdimaaws@gmail.com>
2020-08-20 18:49:50 -0500

    added the slideshow component

---
+++ src/components/content/main-content/MainContent.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './MainContent.scss';
+import Slideshow from '../slide-show/Slideshow';
+
+const MainContent = () => {
+  const images = [
+    {
+      url:
+        'https://images.pexels.com/videos/855907/free-video-855907.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+    },
+    {
+      url: 'https://i.ytimg.com/vi/CmxArNBJHFQ/maxresdefault.jpg'
+    },
+    {
+      url:
+        'https://www.telegraph.co.uk/content/dam/films/2016/12/12/titanic-main-xlarge_trans_NvBQzQNjv4Bqzcd--Xj9PJMVYU0p8x70SFqz9KkK9VrRB9X1d8OfXf0.jpg?imwidth=1200'
+    }
+  ];
+  return (
+    <div className="main-content">
+      <Slideshow images={images} auto={true} showArrows={true} />
+      <div className="grid-movie-title">
+        <div className="movieType">Now Playing</div>
+        <div className="paginate">Paginate</div>
+      </div>
+      {/* display grid components */}
+    </div>
+  );
+};
+
+export default MainContent;
