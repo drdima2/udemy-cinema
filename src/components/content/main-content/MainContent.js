682e549ea8d127d5adc5132e07e84463972fde3d
drdimaaws <drdimaaws@gmail.com>
2020-08-21 13:43:52 -0500

    created grid and rating components

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -3,20 +3,38 @@ import React, { useState } from 'react';
 import './MainContent.scss';
 import Slideshow from '../slide-show/Slideshow';
 import Paginate from '../paginate/Paginate';
+import Grid from '../grid/Grid';
 
 const MainContent = () => {
   const images = [
     {
       url:
-        'https://images.pexels.com/videos/855907/free-video-855907.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+        'https://images.pexels.com/videos/855907/free-video-855907.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 7.5
     },
     {
-      url: 'https://i.ytimg.com/vi/CmxArNBJHFQ/maxresdefault.jpg'
+      url: 'https://i.ytimg.com/vi/CmxArNBJHFQ/maxresdefault.jpg',
+      rating: 8.5
     },
     {
       url:
-        'https://www.telegraph.co.uk/content/dam/films/2016/12/12/titanic-main-xlarge_trans_NvBQzQNjv4Bqzcd--Xj9PJMVYU0p8x70SFqz9KkK9VrRB9X1d8OfXf0.jpg?imwidth=1200'
+        'https://www.telegraph.co.uk/content/dam/films/2016/12/12/titanic-main-xlarge_trans_NvBQzQNjv4Bqzcd--Xj9PJMVYU0p8x70SFqz9KkK9VrRB9X1d8OfXf0.jpg?imwidth=1200',
+      rating: 7.8
+    },
+    {
+      url: 'https://i.ytimg.com/vi/CmxArNBJHFQ/maxresdefault.jpg',
+      rating: 9.7
+    },
+    {
+      url:
+        'https://images.pexels.com/videos/855907/free-video-855907.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      rating: 6.5
+    },
+    {
+      url: 'https://i.ytimg.com/vi/CmxArNBJHFQ/maxresdefault.jpg',
+      rating: 8.5
     }
+
   ];
   const [currentPage, setCurrentPage] = useState(1);
 
@@ -38,7 +56,7 @@ const MainContent = () => {
           <Paginate currentPage={currentPage} totalPages={10} paginate={paginate} />
         </div>
       </div>
-      {/* display grid components */}
+      <Grid images={images}/>
     </div>
   );
 };
